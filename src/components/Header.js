64b12aa93abc83bc5171866519f5c8e93b2c0195
Vitor Lima <vitorliman@gmail.com>
2021-09-16 15:34:28 -0300

    Finaliza requisito 3 e 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      validation: true,
+      user: '',
+    };
+  }
+
+  componentDidMount() {
+    this.myUserName();
+  }
+
+  myUserName = async () => {
+    const userName = await getUser();
+    this.setState({
+      validation: false,
+      user: userName.name,
+    });
+  }
+
   render() {
+    this.myUserName();
+    const { validation, user } = this.state;
+    if (validation) return <Loading />;
     return (
       <header data-testid="header-component">
+        <span data-testid="header-user-name">
+          {user}
+        </span>
         <nav>
           <Link data-testid="link-to-search" to="/search">Search</Link>
           <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
@@ -14,3 +43,6 @@ export default class Header extends Component {
     );
   }
 }
+
+/* Para realizar uma parte do requisito recebi ajuda da Mariana Ferreira
+Segue o repositório: https://github.com/tryber/sd-014-b-project-trybetunes/blob/mariana-trybetunes/src/components/Header.js */
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Album extends Component {
   render() {
     return (
       <div data-testid="page-album">
-        <h2>Album!</h2>
+        <Header />
       </div>
     );
   }
