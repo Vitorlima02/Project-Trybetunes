0c3691f8458bed456b0ab3d94d5a73455b6f3850
Vitor Lima <vitorliman@gmail.com>
2021-09-16 22:06:44 -0300

    Finaliza requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistname: '',
+    };
+  }
+
+  myGetName = ({ target }) => {
+    this.setState({ artistname: target.value });
+  }
+
   render() {
+    const { artistname } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.myGetName }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ artistname.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
