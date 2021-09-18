e5a38a77e3f9632e9483b1bf7324e03c86b74c48
Vitor Lima <vitorliman@gmail.com>
2021-09-18 19:05:13 -0300

    Finaliza requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      artistAlbum: [],
+      collectionName: '',
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.myGetMusicFromApi(id);
+  }
+
+  myGetMusicFromApi = (id) => {
+    getMusics(id).then((result) => this.setState({
+      artistName: result[0].artistName,
+      collectionName: result[0].collectionName,
+      artistAlbum: [...result],
+    }));
+  }
+
   render() {
+    const { artistAlbum, artistName, collectionName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <h2 data-testid="artist-name">{ artistName }</h2>
+        <h5 data-testid="album-name">{ collectionName }</h5>
+        <MusicCard music={ artistAlbum } />
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.string),
+  }).isRequired,
+};
